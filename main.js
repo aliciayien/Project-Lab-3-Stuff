a6dc4990b89e5fe07d899e18faaab14e08a4d39b
AliciaYien <ayien1@my.bcit.ca>
2021-02-10 14:07:03 -0800

    responsive

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector(".mobile-nav").addEventListener("click",
+fucntion(e) {
+    const element = document.querySelector(".main-nav");
+    element.classList.toggle("s-show";)
+    e.preventDefault();
+}, false);
